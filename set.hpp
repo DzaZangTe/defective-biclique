1ee7056bbc2dbf9f2d3c10e69a0ca7c22abbe0b4
DzaZangTe <125554845+DzaZangTe@users.noreply.github.com>
2024-01-13 22:49:50 +0800

    Add files via upload

--- set.hpp
+++ set.hpp
@@ -1,13 +1,13 @@
 std::vector<int> without(const std::vector<int> &S, const int w)
 {
-    std::unordered_set<int> set(S.begin(), S.end());
+    std::set<int> set(S.begin(), S.end());
     set.erase(w);
     return std::vector<int>(set.begin(), set.end());
 }
 
 std::vector<int> without(const std::vector<int> &S, const std::vector<int> &T)
 {
-    std::unordered_set<int> set(S.begin(), S.end());
+    std::set<int> set(S.begin(), S.end());
     for (int t : T)
         set.erase(t);
     return std::vector<int>(set.begin(), set.end());
@@ -15,15 +15,15 @@ std::vector<int> without(const std::vector<int> &S, const std::vector<int> &T)
 
 std::vector<int> add(const std::vector<int> &S, const int w)
 {
-    std::unordered_set<int> set(S.begin(), S.end());
+    std::set<int> set(S.begin(), S.end());
     set.insert(w);
     return std::vector<int>(set.begin(), set.end());
 }
 
 std::vector<int> intersect(const std::vector<int> &S, const std::vector<int> &T)
 {
-    std::unordered_set<int> set(S.begin(), S.end());
-    std::unordered_set<int> res;
+    std::set<int> set(S.begin(), S.end());
+    std::set<int> res;
     for (int t : T)
         if (set.find(t) != set.end())
             res.insert(t);
@@ -37,7 +37,7 @@ std::vector<int> neighbor(const int u)
 
 std::vector<int> neighbor(const std::vector<int> &S)
 {
-    std::unordered_set<int> set;
+    std::set<int> set;
     for (int s : S)
         set.insert(edge[s].begin(), edge[s].end());
     return std::vector<int>(set.begin(), set.end());
@@ -50,7 +50,7 @@ std::vector<int> twoHopNeighbor(const int w)
 
 bool in(const int u, const std::vector<int> &S)
 {
-    std::unordered_set<int> set(S.begin(), S.end());
+    std::set<int> set(S.begin(), S.end());
     return set.find(u) != set.end();
 }
 
